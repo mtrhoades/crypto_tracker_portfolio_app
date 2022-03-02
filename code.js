4d04cfc54232cfb4ac30b8e321d8424d904f3e91
root <mtrhoades0727@gmail.com>
2022-03-01 18:55:50 -1000

    added header/logo and flexed divs

--- code.js
+++ code.js
@@ -2,18 +2,5 @@
 
 
 
-
-
-// change function below to work for all sorts of images called in the url parameter/argument:
-function imageADA(url) {
-    let adaImage = document.createElement('img')
-    adaImage.src = url
-    adaImage.setAttribute('id', 'ADAimage')
-    
-    let div = document.querySelector('#ADA')
-    div.appendChild(adaImage)
-    document.body.append(div)
-}
-
-imageADA('./assets/ADAlogoimage.jpg')
+// add click event listeners to each image for each coin category
 
